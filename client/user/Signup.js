ddf513afe162595758ae0d77938ea695c72918a4
fescobar-2 <fescobar_2@hotmail.com>
2021-02-24 11:53:29 -0300

    Added modules import to SignUp

---
+++ client/user/Signup.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Icon from '@material-ui/core/Icon'
+import { makeStyles } from '@material-ui/core/styles'
+import { create } from '../user/api-user'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContextText from '@material-ui/core/DialogContentText'
+
+
