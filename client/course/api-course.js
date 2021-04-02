2abaeadc99096b611077d59733a09c57fed98f0a
fescobar-2 <fescobar_2@hotmail.com>
2021-04-02 11:43:14 -0400

    Added Course Component without enrollment functionality

---
+++ client/course/api-course.js
@@ -0,0 +1,48 @@
+
+const create = async (params, credentials, course) => {
+  try {
+    let response = await fetch('/api/courses/by/' + params.userId, {
+      method: 'POST',
+      headers:{
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: course
+    })
+      return response.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const read = async (params, signal) => {
+  try {
+    let response = await fetch('/api/courses/'+params.courseId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    return response.json()    
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const listByInstructor = async ( params, credentials, signal ) => {
+  try {
+    let response = await fetch('/api/users/by/' + params.userId,{
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return response.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
